1893593933f762a6490cf0009370157adcdaf63c
Michal Marcincin <40692035+mmarcincin@users.noreply.github.com>
2018-10-07 17:10:36 +0200

    version 0.1

---
+++ HB-books_download.ps1
@@ -0,0 +1,123 @@
+#HB books bulk downloader 0.1 by https://github.com/mmarcincin
+#$links = "links.txt"
+$invocation = (Get-Variable MyInvocation).Value
+$DownloadDirectory = Split-Path $invocation.MyCommand.Path
+$links = "$($DownloadDirectory)\links.txt"
+$DownloadDirectory = "$($DownloadDirectory)\downloads"
+#$DownloadDirectory = "downloads"
+$temp = "$DownloadDirectory\temp"
+
+write-host Download directory`: $DownloadDirectory`n
+
+$ConCountr1=0
+#Test Internet connection
+While (!(Test-Connection -ComputerName humblebundle.com -count 1 -Quiet -ErrorAction SilentlyContinue )) {
+	Write-Host -ForegroundColor Red "Waiting for internet connection to continue..."
+	Start-Sleep -Seconds 10
+	$ConCountr1 +=1
+	If ($ConCountr1 -ge 12) {
+		Write-Host -ForegroundColor Red "Script terminated because of no internet connection/humblebundle response for 120 seconds."
+		Start-Sleep -Seconds 5
+		return
+	}
+}
+
+
+If (!(Test-Path $DownloadDirectory)){
+	New-Item -ItemType directory -Path $DownloadDirectory | Out-Null
+}
+
+If (!(Test-Path $temp)){
+	New-Item -ItemType directory -Path $temp | Out-Null
+}
+Remove-Item "$temp\*" -Recurse
+
+
+If (!(Test-Path $links)){
+	New-Item -ItemType file -Path $links | Out-Null
+}
+
+
+$currentDownload = 0
+$downloadCount = Get-Content $links | Measure-Object -Line | Select -ExpandProperty Lines
+
+Get-Content $links | Foreach-Object {
+	$currentDownload++
+	$host.ui.RawUI.WindowTitle = "D: " + $currentDownload + "/" + $downloadCount
+	
+	$ie = new-object -ComObject "InternetExplorer.Application"
+	$requestUri = $_
+	#$requestUri = "https://www.humblebundle.com/downloads?key=EX7qGbKUfTTEm8vY"
+	#$requestUri
+	#$ie.visible = $true
+	$ie.silent = $true
+	$ie.navigate($requestUri)
+	#while($ie.Busy) { Start-Sleep -Milliseconds 100 }
+	while($ie.Busy -or $ie.ReadyState -ne "4") { Start-Sleep -Milliseconds 100 }
+	
+	while (($ie.Document.getElementsByClassName("whitebox-redux").length -eq "0") -or ($ie.Document.getElementsByClassName("whitebox-redux")[0].innerText.length -eq "0") -or ($ie.Document.getElementsByClassName("icn").length -eq "0") -or ($ie.Document.getElementsByClassName("icn")[0].innerText.length -eq "0")) { Start-Sleep -Milliseconds 100 }
+	
+	$doc = $ie.Document
+	$docTitle = $doc.getElementsByTagName("title")[0].innerText.trim()
+	$bundleName = $docTitle.substring(0, $docTitle.lastIndexOf("(") - 1)
+	$bundleName = [Text.Encoding]::ASCII.GetString([Text.Encoding]::GetEncoding("Cyrillic").GetBytes($bundleName))
+	$bundleTitle = $bundleName -replace '[^a-zA-Z0-9/_/''/\-/ ]', '_'
+	$bundleTitle = $bundleTitle -replace '/', '_'
+	write-host ==============================================================
+	write-host $currentDownload "/" $downloadCount - $bundleTitle
+	write-host $requestUri
+	write-host --------------------------------------------------------------
+	
+	If (!(Test-Path $temp\$bundleTitle)){
+			New-Item -ItemType directory -Path $temp\$bundleTitle | Out-Null
+		}
+	
+	$hb = $doc.getElementsByClassName("icn")
+	for ($i = 0; $i -lt $hb.length; $i++) {
+		$curTitle = $hb[$i].parentNode
+		$humbleName = [Text.Encoding]::ASCII.GetString([Text.Encoding]::GetEncoding("Cyrillic").GetBytes($curTitle.getAttribute("data-human-name")))
+		$humbleTitle = $humbleName -replace '[^a-zA-Z0-9/_/''/\-/ ]', '_'
+		$humbleTitle = $humbleTitle -replace '/', '_'
+		
+		$chunkLength = $hb.length
+		$chunkNumber = $i+1
+		$host.ui.RawUI.WindowTitle = "D: " + $currentDownload + "/" + $downloadCount +" `| "+ $chunkNumber + "/" + $chunkLength
+		
+		write-host `n$chunkNumber / $chunkLength - $humbleTitle
+		
+		If (!(Test-Path $DownloadDirectory\$bundleTitle\$humbleTitle)){
+			New-Item -ItemType directory -Path $temp\$bundleTitle\$humbleTitle | Out-Null
+		}
+		
+		if ($curTitle.getElementsByClassName("download-buttons")[0].innerHTML.length -gt "0") {
+			$downLabels = $curTitle.getElementsByClassName("download-buttons")[0].getElementsByClassName("label")
+			for ($j = 0; $j -lt $downLabels.length; $j++) {
+				$curLabel = $downLabels[$j].innerHTML
+				
+				$downLink = $downLabels[$j].parentNode.getElementsByClassName("a")[0].href
+				$downName = $downLink.split("?")[0].split("/")
+				$downTitle = $downName[$downName.length-1]
+				write-host $curLabel - $downTitle
+				$wc = New-Object System.Net.WebClient
+				$downDest = "$temp\$bundleTitle\$humbleTitle\$downTitle"
+				#$downDest
+				If (!(Test-Path $DownloadDirectory\$bundleTitle\$humbleTitle\$downTitle)){
+					$wc.DownloadFile($downLink, $downDest)
+				} else {
+					write-host File downloaded already, skipping...
+				}
+			}
+			If (!(Test-Path $DownloadDirectory\$bundleTitle)){
+			New-Item -ItemType directory -Path $DownloadDirectory\$bundleTitle | Out-Null
+			}
+			Move-Item -Path $temp\$bundleTitle\* -Destination $DownloadDirectory\$bundleTitle
+			pause
+		}
+	}
+	Remove-Item "$temp\*" -Recurse
+	write-host ==============================================================
+	$ie.quit()
+	Start-Sleep -Seconds 2
+}
+$host.ui.RawUI.WindowTitle = "D: " + $currentDownload + "/" + $downloadCount +" `| Finished"
+pause
--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # HB books bulk downloader
+HB books bulk downloader 0.1
+----------------------
+It's a powershell script which allows you to download books from humble bundle pages (https://www.humblebundle.com/downloads?key=XXXXXXXXXXXXXXXX) in bulk.
+It uses Internet Explorer instance to retrieve the links so all you need to do is login to humble bundle through the internet explorer and that's it.
+
+Right now the script puts each book into its own folder and all versions inside. 
+I'll add some options later, like download only some versions (pdf, epub, etc..) and maybe different folder sorting by book version instead of book name (for example, all pdf in one folder).
+
+
+Powershell ExecutionPolicy change
+----------------------
+start RUN.bat to launch the script
+for editing the script itself open HB-books_download.ps1 in notepad (or notepad++,etc...)
+
+If the window closes fast after starting RUN.bat (or it doesn't show on startup when using that option): 
+1. Go to any folder (file explorer) and choose file > open windows powershell > 
+   > open windows powershell as administrator.
+2. In the Windows PowerShell window type: get-ExecutionPolicy.
+3. If you are geting the 'Restricted' text, type: set-ExecutionPolicy RemoteSigned,
+   then just confirm with y for yes.
+4. After that the RUN.bat should work as intented.
+
+If you'd like to create a shortcut for the script, you just need to make shortcut of RUN.bat file.
